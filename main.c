cd2aace9b9085f08629221a1cfd79c84853b5cac
Thanaphon0086 <geezxcvbnmgee@gmail.com>
2020-08-18 10:18:24 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  float R,H,ANS;
+  printf("Enter R ");
+  scanf("%f", &R);
+  printf("Enter H ");
+  scanf("%f", &H);
+ANS = (2*22/7*R)*H+2*(22/7*R*R);
+printf("ANSWHR IS %0.2f", ANS); 
+  return 0;
+}
